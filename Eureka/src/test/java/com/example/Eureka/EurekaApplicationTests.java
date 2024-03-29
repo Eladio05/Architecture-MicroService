e3db406113d6cde5c8e5ddfb4c7247416334f408
MERIEM DOGHECHE <meriem.dogheche@gmail.com>
2024-03-29 15:56:28 +0100

    ajout d'Eureka

---
+++ Eureka/src/test/java/com/example/Eureka/EurekaApplicationTests.java
@@ -0,0 +1,14 @@
+package com.example.Eureka;
+
+import org.springframework.boot.test.context.SpringBootTest;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+
+@SpringBootTest(classes = EurekaServerApplication.class)
+class EurekaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
