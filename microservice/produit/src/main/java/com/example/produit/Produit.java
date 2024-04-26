ef95a84a916f13062e24bd342c462573822d36a2
Eladio05 <eliottp59@sfr.fr>
2024-04-26 09:27:25 +0200

    Ajout Image et description docker

--- microservice/produit/src/main/java/com/example/produit/Produit.java
+++ microservice/produit/src/main/java/com/example/produit/Produit.java
@@ -5,25 +5,31 @@ import jakarta.persistence.*;
 @Table(name = "Produit")
 public class Produit {
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY) // Génère l'ID autom
-    // atiquement
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // Génère l'ID automatiquement
     @Column(name = "idproduit", nullable = false)
     private int idProduit;
 
     @Column(name = "nomproduit", nullable = false)
     private String nomProduit;
 
+    @Column(name = "description", nullable = false)
+    private String description;
 
     @Column(name = "prix", nullable = false)
     private double prix;
 
+    @Column(name = "urlImage", nullable = false)
+    private String urlImage;
+
     public Produit() {
     }
 
-    public Produit(int idProduit, String nomProduit, double prix) {
+    public Produit(int idProduit, String nomProduit, String description, double prix, String urlImage) {
         this.idProduit = idProduit;
         this.nomProduit = nomProduit;
+        this.description = description;
         this.prix = prix;
+        this.urlImage = urlImage;
     }
 
     // Getters et setters
@@ -44,6 +50,14 @@ public class Produit {
         this.nomProduit = nomProduit;
     }
 
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
     public double getPrix() {
         return prix;
     }
@@ -52,12 +66,22 @@ public class Produit {
         this.prix = prix;
     }
 
+    public String getUrlImage() {
+        return urlImage;
+    }
+
+    public void setUrlImage(String urlImage) {
+        this.urlImage = urlImage;
+    }
+
     @Override
     public String toString() {
         return "Produit{" +
                 "idProduit=" + idProduit +
                 ", nomProduit='" + nomProduit + '\'' +
+                ", description='" + description + '\'' +
                 ", prix=" + prix +
+                ", urlImage='" + urlImage + '\'' +
                 '}';
     }
 }
